64bac208cbd07c4e38cbf4ef67ce3898088ebede
danCode-s <dancodes9@gmail.com>
2023-12-21 19:45:23 +0500

    TicTacToe; some implementation left

---
+++ tic-tac-toe.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <ctime>
+
+void drawBoard(char *spaces);
+void playerMove(char *spaces, char player);
+void computerMove(char *spaces, char computer);
+bool checkWinner(char *spaces, char player,  char computer);
+bool checkTie(char *spaces, char player, char computer);
+
+
+
+int main(){
+
+    char spaces[9] = {' ', ' ',' ', ' ',' ', ' ',' ', ' ',' '};
+    char player = 'X';
+    char computer = 'O';
+    bool running = true;
+
+    drawBoard(spaces);
+
+    while(running){
+        playerMove(spaces, player);
+        drawBoard(spaces);
+
+        computerMove(spaces, computer);
+        drawBoard(spaces);
+    }
+
+};
+
+void drawBoard(char *spaces){
+    std::cout << "\n";
+    std::cout << "     |     |     " << "\n";
+    std::cout << "  " << spaces[0] << "  |  " << spaces[1] << "  |  " << spaces[2] << "  " << "\n";
+    std::cout << "_____|_____|_____" << "\n";
+    std::cout << "     |     |     " << "\n";
+    std::cout << "  " << spaces[3] << "  |  " << spaces[4] << "  |  " << spaces[5] << "  " << "\n";
+    std::cout << "_____|_____|_____" << "\n";
+    std::cout << "     |     |     " << "\n";
+    std::cout << "  " << spaces[6] << "  |  " << spaces[7] << "  |  " << spaces[8] << "  " << "\n";
+    std::cout << "     |     |     " << "\n";
+    std::cout << "\n";
+
+};
+
+
+void playerMove(char *spaces, char player){
+    int number;
+    do
+    {
+        std::cout << "Enter a spot to place marker(1-9): ";
+        std::cin >> number;
+        number--;
+        
+        if(spaces[number] == ' '){
+            spaces[number] = player;
+            break;
+        }
+    } while (!number > 0 || !number < 8);
+    
+
+};
+void computerMove(char *spaces, char computer){
+    int number;
+    srand(time(NULL));
+    
+    
+    while(true){
+        number = rand() % 9;
+        if(spaces[number] == ' '){
+            spaces[number] = computer;
+            break;
+        };
+    }
+
+};
+bool checkWinner(char *spaces, char player,  char computer){
+    return 0;
+};
+bool checkTie(char *spaces, char player, char computer){
+    return 0;
+};
